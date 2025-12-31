dfdaff07b85cd22b031c7610bb516cb4261fe8a3
mhdirfndzky <irfndzky@gmail.com>
2025-12-31 18:01:48 +0700

    refactor: remove webpack caching and optimization for faster builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Enable webpack cache for faster builds
-    config.cache = {
-      type: 'filesystem',
-      compression: 'gzip',
-    };
-    
-    // Optimize bundle size
-    config.optimization = {
-      ...config.optimization,
-      usedExports: true,
-      sideEffects: true,
-    };
-    
-    return config;
-  },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
@@ -27,9 +11,9 @@ const nextConfig = {
   },
   // Enable SWC minification
   swcMinify: true,
-  // Reduce bundle size
+  // Reduce bundle size - optimize react-icons import
   experimental: {
-    optimizePackageImports: ['react-icons', 'framer-motion'],
+    optimizePackageImports: ['react-icons'],
   },
 };
 
